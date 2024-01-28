dc1ca89cc2f4ea602528527a0e87760f4523b2f8
Mikołaj Mrózek <m.mik311@gmail.com>
2024-01-28 16:26:29 +0100

    fix: adjust atomic compoenents types

--- src/renderer/src/components/base/Button/Button.tsx
+++ src/renderer/src/components/base/Button/Button.tsx
@@ -1,3 +1,4 @@
+import clsx from 'clsx';
 import { FC, ReactNode } from 'react';
 
 type ButtonVariant = 'primary' | 'secondary' | 'tertiary';
@@ -6,7 +7,7 @@ type ButtonProps = {
   children?: ReactNode;
   onClick?: () => void;
   variant?: ButtonVariant;
-};
+} & JSX.IntrinsicElements['button'];
 
 export const Button: FC<ButtonProps> = ({ variant, ...props }) => {
   switch (variant) {
@@ -21,38 +22,41 @@ export const Button: FC<ButtonProps> = ({ variant, ...props }) => {
   }
 };
 
-const PrimaryButton: FC<Omit<ButtonProps, 'variant'>> = ({ children, onClick }) => {
+const PrimaryButton: FC<Omit<ButtonProps, 'variant'>> = (props) => {
   return (
     <button
-      onClick={onClick}
+      {...props}
       tabIndex={0}
-      className="h-[2.22rem] px-4 box-border bg-blue-500 rounded-sm text-zinc-50 font-medium focus:ring-2 focus:ring-blue-500 focus:ring-offset-1 focus:outline-none hover:bg-blue-600 active:bg-blue-700 active:ring-0 active:ring-offset-0 transition-colors duration-200 ease-in-out"
-    >
-      {children}
-    </button>
+      className={clsx(
+        'h-[2.22rem] px-4 box-border bg-blue-500 rounded-sm text-zinc-50 font-medium focus:ring-2 focus:ring-blue-500 focus:ring-offset-1 focus:outline-none hover:bg-blue-600 active:bg-blue-700 active:ring-0 active:ring-offset-0 transition-colors duration-200 ease-in-out',
+        props.className
+      )}
+    />
   );
 };
 
-const SecondaryButton: FC<Omit<ButtonProps, 'variant'>> = ({ children, onClick }) => {
+const SecondaryButton: FC<Omit<ButtonProps, 'variant'>> = (props) => {
   return (
     <button
-      onClick={onClick}
+      {...props}
       tabIndex={0}
-      className="h-[2.22rem] px-4 box-border border border-blue-500 bg-blue-500/0 rounded-sm text-blue-500 font-medium focus:ring-2 focus:ring-blue-500 focus:ring-offset-1 focus:outline-none hover:bg-blue-500/15 active:bg-blue-500/30 active:ring-0 active:ring-offset-0 transition-colors duration-200 ease-in-out"
-    >
-      {children}
-    </button>
+      className={clsx(
+        'h-[2.22rem] px-4 box-border border border-blue-500 bg-blue-500/0 rounded-sm text-blue-500 font-medium focus:ring-2 focus:ring-blue-500 focus:ring-offset-1 focus:outline-none hover:bg-blue-500/15 active:bg-blue-500/30 active:ring-0 active:ring-offset-0 transition-colors duration-200 ease-in-out',
+        props.className
+      )}
+    />
   );
 };
 
-const TertiaryButton: FC<Omit<ButtonProps, 'variant'>> = ({ children, onClick }) => {
+const TertiaryButton: FC<Omit<ButtonProps, 'variant'>> = (props) => {
   return (
     <button
-      onClick={onClick}
+      {...props}
       tabIndex={0}
-      className="h-[2.2rem] px-4 box-border bg-blue-500/0 rounded-sm text-blue-500 font-medium focus:ring-2 focus:ring-blue-500 focus:ring-offset-1 focus:outline-none hover:bg-blue-500/15 active:bg-blue-500/30 active:ring-0 active:ring-offset-0 transition-colors duration-200 ease-in-out"
-    >
-      {children}
-    </button>
+      className={clsx(
+        'h-[2.2rem] px-4 box-border bg-blue-500/0 rounded-sm text-blue-500 font-medium focus:ring-2 focus:ring-blue-500 focus:ring-offset-1 focus:outline-none hover:bg-blue-500/15 active:bg-blue-500/30 active:ring-0 active:ring-offset-0 transition-colors duration-200 ease-in-out',
+        props.className
+      )}
+    />
   );
 };
