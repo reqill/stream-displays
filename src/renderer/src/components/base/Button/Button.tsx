58a062e778c26df1054d30aead862bde92dbb159
Mikołaj Mrózek <m.mik311@gmail.com>
2024-01-28 16:20:35 +0100

    style: change transitions and looks of base dialog

--- src/renderer/src/components/base/Button/Button.tsx
+++ src/renderer/src/components/base/Button/Button.tsx
@@ -26,7 +26,7 @@ const PrimaryButton: FC<Omit<ButtonProps, 'variant'>> = ({ children, onClick }) 
     <button
       onClick={onClick}
       tabIndex={0}
-      className="h-[2.2rem] px-4 box-border bg-blue-500 rounded-sm text-zinc-50 font-medium focus:ring-2 focus:ring-blue-500 focus:ring-offset-1 focus:outline-none hover:bg-blue-600 active:bg-blue-700 active:ring-0 active:ring-offset-0 transition-colors duration-200 ease-in-out"
+      className="h-[2.22rem] px-4 box-border bg-blue-500 rounded-sm text-zinc-50 font-medium focus:ring-2 focus:ring-blue-500 focus:ring-offset-1 focus:outline-none hover:bg-blue-600 active:bg-blue-700 active:ring-0 active:ring-offset-0 transition-colors duration-200 ease-in-out"
     >
       {children}
     </button>
@@ -38,7 +38,7 @@ const SecondaryButton: FC<Omit<ButtonProps, 'variant'>> = ({ children, onClick }
     <button
       onClick={onClick}
       tabIndex={0}
-      className="h-[2.2rem] px-4 box-border border border-blue-500 bg-blue-500/0 rounded-sm text-blue-500 font-medium focus:ring-2 focus:ring-blue-500 focus:ring-offset-1 focus:outline-none hover:bg-blue-500/15 active:bg-blue-500/30 active:ring-0 active:ring-offset-0 transition-colors duration-200 ease-in-out"
+      className="h-[2.22rem] px-4 box-border border border-blue-500 bg-blue-500/0 rounded-sm text-blue-500 font-medium focus:ring-2 focus:ring-blue-500 focus:ring-offset-1 focus:outline-none hover:bg-blue-500/15 active:bg-blue-500/30 active:ring-0 active:ring-offset-0 transition-colors duration-200 ease-in-out"
     >
       {children}
     </button>
