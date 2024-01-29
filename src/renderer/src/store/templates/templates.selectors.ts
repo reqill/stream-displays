7601e8e9608b38c4dd2a20c19f2786b085afc1c8
Mikołaj Mrózek <m.mik311@gmail.com>
2024-01-29 18:08:19 +0100

    refactor: template view redux is now based on array

--- src/renderer/src/store/templates/templates.selectors.ts
+++ src/renderer/src/store/templates/templates.selectors.ts
@@ -1,12 +1,3 @@
 import { RootState } from '../index';
 
-export const getTemplateByIdSelector = (templateId: string) => (state: RootState) => {
-  return state.templates[templateId];
-};
-
-export const getAllTemplatesSelector = (state: RootState) => {
-  return state.templates;
-};
-export const getAllTemplatesInArraySelector = (state: RootState) => {
-  return Object.values(state.templates);
-};
+export const getAllTemplatesSelector = (state: RootState) => state.templates.templates;
